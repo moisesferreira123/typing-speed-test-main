c81ae97fb98f22fb7a78f6bf45d62b3d01c41324
Moisés Ferreira <moisesaoaoao@gmail.com>
2026-02-02 17:58:42 -0300

    Fixing baseURL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-// https://typing-speed-test-backend-1ddt.onrender.com
+
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://typing-speed-test-backend-1ddt.onrender.com",
   timeout: 12000
 });
 
